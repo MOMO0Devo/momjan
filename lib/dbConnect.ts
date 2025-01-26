9d2f432cef936a839f3e3715afd07cab6f2d8172
MOMO0Devo <mamkm8276@gmail.com>
2025-01-26 20:40:45 +0100

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 async function dbConnect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI!)
+    await mongoose.connect("mongodb+srv://momo123:momo123@cluster0.bnbzv.mongodb.net/dockerproj")
   } catch (error) {
     throw new Error('Connection failed!')
   }
